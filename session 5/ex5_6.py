c37228ecea0e1d93942e52693543ae22c10030f8
DESKTOP-DVMGHEA\Veldora Tempest <minh101202@gmail.com>
2017-09-06 01:02:41 +0700

    session 5

---
+++ session 5/ex5_6.py
@@ -0,0 +1,13 @@
+##5
+def remove_dollar_sign(s):
+    str(s)
+    s=s.replace("$","")
+    return s
+
+
+##6
+string_with_no_dollars = remove_dollar_sign("$80% percent of $life is to show $up")
+if string_with_no_dollars == "80% percent of life is to show up":
+    print("Your function is correct")
+else:
+    print("Oops, there's a bug")
